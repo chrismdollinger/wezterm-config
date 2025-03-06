534e4715874b7bd082a165bb7a554f0fce94e118
Christopher Dollinger <4001728+chrismdollinger@users.noreply.github.com>
2025-03-06 13:45:43 -0500

    Adding section for OS-specific settings

--- wezterm.lua
+++ wezterm.lua
@@ -3,18 +3,23 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.color_scheme = 'AdventureTime'
-config.font = wezterm.font_with_fallback {
-    'Cascadia Code NF',
-    'CaskaydiaCove Nerd Font Mono'
-}
-native_macos_fullscreen_mode = false
-config.default_prog = { 'nu' }
 
-config.launch_menu = {
-    {
-        label = 'Oracle Sql Developer Command Line (SqlCl)',
-        args = { 'sql', '/nolog' },
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+    config.font = wezterm.font('Cascadia Code NF')
+    config.default_prog = { 'nu' }
+
+    config.launch_menu = {
+        {
+            label = 'Oracle Sql Developer Command Line (SqlCl)',
+            args = { 'sql', '/nolog' },
+        }
     }
-}
+
+end
+
+if wezterm.target_triple == 'aarch64-apple-darwin' then
+    config.font = wezterm.font('CaskaydiaCove Nerd Font Mono')
+    native_macos_fullscreen_mode = false
+end
 
 return config
